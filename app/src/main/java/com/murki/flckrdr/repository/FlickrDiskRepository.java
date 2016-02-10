134b09b19c1cac9860d4aa8dbbe0f31af58f5311
mjuarez <mjuarez@yammer-inc.com>
2016-02-09 18:56:05 -0800

    Removing dependency from RxPreferences since we were not really taking advantage from it

--- app/src/main/java/com/murki/flckrdr/repository/FlickrDiskRepository.java
+++ app/src/main/java/com/murki/flckrdr/repository/FlickrDiskRepository.java
@@ -3,10 +3,7 @@ package com.murki.flckrdr.repository;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.support.annotation.NonNull;
 
-import com.f2prateek.rx.preferences.Preference;
-import com.f2prateek.rx.preferences.RxSharedPreferences;
 import com.fernandocejas.frodo.annotation.RxLogObservable;
 import com.murki.flckrdr.model.RecentPhotosResponse;
 import com.squareup.moshi.JsonAdapter;
@@ -23,34 +20,19 @@ public class FlickrDiskRepository {
 
     private final static String RECENT_PHOTOS_RESPONSE_KEY = "com.murki.flckrdr.model.RecentPhotosResponse_key";
 
-    private final RxSharedPreferences rxPreferences;
+    private final SharedPreferences sharedPreferences;
     private final JsonAdapter<Timestamped<RecentPhotosResponse>> flickrPhotosJsonAdapter;
 
     public FlickrDiskRepository(Context context) {
-        rxPreferences = RxSharedPreferences.create(PreferenceManager.getDefaultSharedPreferences(context));
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
         Moshi moshi = new Moshi.Builder().build();
         Type adapterType = Types.newParameterizedType(Timestamped.class, RecentPhotosResponse.class);
         flickrPhotosJsonAdapter = moshi.adapter(adapterType);
     }
 
-//    @RxLogObservable
-//    public Observable<Void> savePhotos(final RecentPhotosResponse photos) {
-//        return Observable.create(new Observable.OnSubscribe<Void>() {
-//            @Override
-//            public void call(Subscriber<? super Void> subscriber) {
-//                try {
-//                    rxPreferences.getObject(RECENT_PHOTOS_RESPONSE_KEY, flickrPhotosAdapter).set(photos);
-//                    subscriber.onNext(null);
-//                    subscriber.onCompleted();
-//                } catch (Exception ex) {
-//                    subscriber.onError(ex);
-//                }
-//            }
-//        });
-//    }
-
     public void savePhotos(Timestamped<RecentPhotosResponse> photos) {
-        rxPreferences.getObject(RECENT_PHOTOS_RESPONSE_KEY, flickrPhotosAdapter).set(photos);
+        String serializedPhotoList = flickrPhotosJsonAdapter.toJson(photos);
+        sharedPreferences.edit().putString(RECENT_PHOTOS_RESPONSE_KEY, serializedPhotoList).commit();
     }
 
     @RxLogObservable
@@ -59,7 +41,8 @@ public class FlickrDiskRepository {
             @Override
             public void call(Subscriber<? super Timestamped<RecentPhotosResponse>> subscriber) {
                 try {
-                    Timestamped<RecentPhotosResponse> photos = rxPreferences.getObject(RECENT_PHOTOS_RESPONSE_KEY, flickrPhotosAdapter).get();
+                    String serializedPhotoList = sharedPreferences.getString(RECENT_PHOTOS_RESPONSE_KEY, "");
+                    Timestamped<RecentPhotosResponse> photos = flickrPhotosJsonAdapter.fromJson(serializedPhotoList);
                     subscriber.onNext(photos);
                     subscriber.onCompleted();
                 } catch (Exception ex) {
@@ -68,25 +51,4 @@ public class FlickrDiskRepository {
             }
         });
     }
-
-    private final Preference.Adapter<Timestamped<RecentPhotosResponse>> flickrPhotosAdapter = new Preference.Adapter<Timestamped<RecentPhotosResponse>>() {
-        @Override
-        public Timestamped<RecentPhotosResponse> get(@NonNull String key, @NonNull SharedPreferences sharedPreferences) {
-            Timestamped<RecentPhotosResponse> photos = null;
-            try {
-                String serializedPhotoList = sharedPreferences.getString(key, "");
-                photos = flickrPhotosJsonAdapter.fromJson(serializedPhotoList);
-            } catch (Exception e) {
-                e.printStackTrace(); // TODO: Pass error to observable chain
-            }
-            return photos;
-        }
-
-        @Override
-        public void set(@NonNull String key, @NonNull Timestamped<RecentPhotosResponse> flickrPhotos, @NonNull SharedPreferences.Editor editor) {
-            String serializedPhotoList = flickrPhotosJsonAdapter.toJson(flickrPhotos);
-            editor.putString(key, serializedPhotoList);
-        }
-    };
-
 }
